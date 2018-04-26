53de24b0c8a33dab87a3da76b84401f817b51fed
Robert Laws <roblaws@me.com>
2018-04-26 07:16:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showRepositories);
+  req.open("GET", "https://api.github.com/users/octocat/repos");
+  req.send();
+}
+
